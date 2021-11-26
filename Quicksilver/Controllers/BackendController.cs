0c444b75d80f5b72104e69f8fc2546df8e06277a
Chetan Suthar <chetandstr@gmail.com>
2021-11-26 19:32:10 -0400

    testss

--- Quicksilver/Controllers/BackendController.cs
+++ Quicksilver/Controllers/BackendController.cs
@@ -12,7 +12,7 @@ namespace Quicksilver.Controllers
     public class BackendController : Controller
     {
 
-        //this is test commit again
+        //this is test 
         private readonly CommonRepository commonRepository = new CommonRepository();
         public IActionResult Index()
         {
